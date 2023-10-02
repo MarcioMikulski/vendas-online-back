cf29f092b36d1b98d3015fba46ac91fa7489a098
Marcio Mikulski <mugemikulski@gmail.com>
2023-10-02 20:20:10 -0300

    feat: create token JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: process.env.JWT_EXPIRES_IN,
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
